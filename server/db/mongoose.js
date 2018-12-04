108baa7d87a7a0bf4078533dc601a46cce77c14b
Ferrymania <904655716@qq.com>
2018-12-04 20:08:01 +0800

    add test config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.Promise = Promise;
 
-mongoose.connect( process.env.MONGODB_URL||'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }).then(()=>{
-    
+mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true }).then(()=>{
+
 });
 
 module.exports = {mongoose};
